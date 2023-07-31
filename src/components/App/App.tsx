a1ee36d8166620ea12b4ec99a7d6fa3f20cab48e
Andrea Prat <andr8ide@gmail.com>
2023-07-31 23:13:30 +0200

    Add App.tsx with types and QuestionStructure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { QuestionStructure } from "../../types";
+import Question from "../Question/Question";
+
+const App = (): React.ReactElement => {
+  const initialQuestions: QuestionStructure[] = [
+    {
+      id: 0,
+      question: "La tortilla:",
+      answer: "",
+    },
+    {
+      id: 1,
+      question: "En verano:",
+      answer: "",
+    },
+  ];
+
+  const [questions, setQuestions] = useState(initialQuestions);
+
+  return (
+    <div className="container">
+      {questions.map((item) => (
+        <Question key={item.id} question={item} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
